21684332166a35502cd28e460f9756455bb5b57d
karanvaja <vajakaran0999@gmail.com>
2026-08-27 06:21:36 -0400

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,7 +1,16 @@
 from flask_frozen import Freezer
 from app import app
 
+# Freezer ma badhi routes freeze na thay etle skip karo
 freezer = Freezer(app)
+
+# POST routes ne skip karo (email send, resume download)
+@freezer.register_generator
+def skip_routes():
+    # Only generate GET routes
+    yield '/'
+    yield '/download_resume'
+    # '/send_email' ne skip karo
 
 if __name__ == '__main__':
     freezer.freeze()
